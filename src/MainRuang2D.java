21be6ccafa0535915c887d7ff6a22ac88bde219c
fatimatuzzahro <fatym_z@yahoo.com>
2016-04-18 19:21:49 +0700

    Sourcecode

---
+++ src/MainRuang2D.java
@@ -0,0 +1,11 @@
+public class MainRuang2D {
+    public static void main(String[] args) {
+        System.out.println("Luas Lingkaran     = "+Ruang2D.LuasLingkaran());
+        System.out.println("Keliling Lingkaran = "+Ruang2D.KelilingLingkaran());
+        System.out.println("Luas Segitiga      = "+Ruang2D.LuasSegitiga());
+        System.out.println("Keliling Segitiga  = "+Ruang2D.KelilingSegitiga());
+        System.out.println("Luas SegiEmpat     = "+Ruang2D.LuasSegiempat());
+        System.out.println("Keliling SegiEmpat = "+Ruang2D.KelilingSegiempat());
+    }
+    
+}
