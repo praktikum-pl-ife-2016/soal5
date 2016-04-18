39582d325e0ecceff3b7a611251f86824f584ba4
muhridwan97 <muh_ridwan97@yahoo.co.id>
2016-04-18 20:05:30 +0700

    tugas 5

---
+++ src/MainRuang2D.java
@@ -0,0 +1,17 @@
+
+public class MainRuang2D {
+    public static void main(String[] args) {
+        System.out.println("SEGITIGA : ");
+        System.out.println("Luas Segitiga      = "+Ruang2D.LuasSegitiga());
+        System.out.println("Keliling Segitiga  = "+Ruang2D.KelilingSegitiga());
+        System.out.println("");
+        System.out.println("PERSEGI PANJANG : ");
+        System.out.println("Luas SegiEmpat     = "+Ruang2D.LuasPP());
+        System.out.println("Keliling SegiEmpat = "+Ruang2D.KelilingPP());
+        System.out.println("");
+        System.out.println("LINGKARAN");
+        System.out.println("Luas Lingkaran     = "+Ruang2D.LuasLing());
+        System.out.println("Keliling Lingkaran = "+Ruang2D.KelilingLing());
+    }
+}
+
