8953a70830b8f118fd3b207a7d951606a0c425c9
adikurniawan1996 <adikurniawan1996@gmail.com>
2016-04-18 15:51:23 +0700

    SOURCE CODE

---
+++ src/MainRuang2D.java
@@ -0,0 +1,10 @@
+public class MainRuang2D {
+    public static void main(String[] args) {
+        System.out.println("Luas Segi Tiga      : "+Ruang2D.LuasSegiTiga(5, 2));
+        System.out.println("Keliling Segi Tiga  : "+Ruang2D.KelilingSegiTiga(1, 2, 3));
+        System.out.println("Luas Segi Empat     : "+Ruang2D.luasSegiEmpat(5, 3));
+        System.out.println("Keliling Segi Empat : "+Ruang2D.kelilingSegiEmpat(4,5));
+        System.out.println("Luas Lingkaran      : "+Ruang2D.luasLingkaran(10));
+        System.out.println("Keliling Lingkaran  : "+Ruang2D.kelilingLingkaran(7));       
+    }
+}
