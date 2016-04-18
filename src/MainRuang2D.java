b2008dc10448fc68c7ec01dac7f7bd1e332ad735
MahardikaYurist <mtreujurist04@gmail.com>
2016-04-18 16:29:27 +0700

    Pengumpulan Soal 5

---
+++ src/MainRuang2D.java
@@ -0,0 +1,13 @@
+
+
+public class MainRuang2D {
+        public static void main(String[] args) {
+            System.out.println("Keliling Segitiga : "+Ruang2D.KelilingSegitiga(10, 10, 10));
+            System.out.println("Luas Segitiga : "+Ruang2D.luasSegitiga(10, 6));
+            System.out.println("Keliling Segiempat : "+Ruang2D.kelilingSegiempat(16, 6));
+            System.out.println("Luas Segiempat : "+Ruang2D.luasSegiempat(16, 6));
+            System.out.println("Keliling Lingkaran  : "+Ruang2D.kelilingLingkarang(6));
+            System.out.println("LuasLingkaran : "+Ruang2D.luasLingkaran(12));
+        }
+    
+}
