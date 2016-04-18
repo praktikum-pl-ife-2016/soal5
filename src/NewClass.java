e01f3238fc41a60f8d95e47723f6be632456c568
Alvin <alvinandaakhdori@gmail.com>
2016-04-18 22:50:06 +0700

    Pengumpulan Soal 5

---
+++ src/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest-RVUsNg
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        Ruang2D.KelilingLingkaran(10);
+        Ruang2D.LuasLingkaran(100);
+        Ruang2D.LuasSegiempat(20, 30);
+        Ruang2D.LuasSegitiga(12, 13);
+        Ruang2D.KelilingSegitempat(20, 50);
+        Ruang2D.KelilingSegitiga(10, 6);
+    }
+}
