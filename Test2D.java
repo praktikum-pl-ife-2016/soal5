e37c360b983f29fc859b93aa46d2c99c7e2547ec
efriza11 <efriza88@gmail.com>
2016-04-18 20:09:32 +0700

    src

---
+++ Test2D.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mm;
+
+
+
+/**
+ *
+ * @author guest-ARLjF4
+ */
+public class Test2D {
+
+     public static void main(String[] args) {
+            System.out.println("Luas Segitiga  : "+Ruang2D.LSegitiga(3, 4));
+            System.out.println("Luas Segiempat : "+Ruang2D.LSegiEmpat(3, 4));
+            System.out.println("Luas Lingkaran : "+Ruang2D.LuasLing(4));
+            System.out.println("Keliling Segitiga  : "+Ruang2D.KSegiTiga(3, 4, 5));
+            System.out.println("Keliling SegiEmpat : "+Ruang2D.KSegiEmpat(3, 4));
+            System.out.println("Keliling Lingkaran : "+Ruang2D.KeLing(4));
+    }
+}
