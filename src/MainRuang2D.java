41cbf5f71301cd3153f528de23225e9c0499e294
THAREQ IBRAHIM <thareq1224@gmail.com>
2016-04-19 01:11:41 +0700

    Source Code Tugas

---
+++ src/MainRuang2D.java
@@ -0,0 +1,15 @@
+public class MainRuang2D {
+    
+    public static void main(String[] args) {
+   
+        System.out.println("Luas Segitiga : " + Ruang2D.hitungLuasSegitiga(5, 6));
+        System.out.println("Keliling Segitiga : " + Ruang2D.hitungKelilingSegitiga(5, 6, 8));
+        System.out.println("Luas Segiempat : " + Ruang2D.hitungLuasSegiempat(7, 3));
+        System.out.println("Keliling Segiempat : " + Ruang2D.hitungKelilingSegiempat(7, 3));
+        System.out.println("Luas Lingkaran : " + Ruang2D.hitungLuasLingkaran(9));
+        System.out.println("Keliling Lingkaran : " + Ruang2D.hitungKelilingLingkaran(9));
+        
+}
+    
+    
+}
