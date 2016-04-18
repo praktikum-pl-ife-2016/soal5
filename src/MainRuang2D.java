c5394921ac780ab5fa2e460871731a17ede44f5e
nindasilvia <NINDASILVIATRICAHYANI@gmail.com>
2016-04-18 20:32:42 +0700

    source code

---
+++ src/MainRuang2D.java
@@ -0,0 +1,15 @@
+
+package ruang2d;
+
+import java.util.Scanner;
+
+public class MainRuang2D {
+  public static void main (String[]args) {
+     Ruang2D.KelilingLingkaran(12);
+     Ruang2D.KelilingSegiempat(24,5);
+     Ruang2D.KelilingSegitiga(12, 6,8);
+     Ruang2D.LuasLingkaran(28);
+     Ruang2D.LuasSegiempat(30);
+     Ruang2D.LuasSegitiga(6, 8 );
+
+  }}
