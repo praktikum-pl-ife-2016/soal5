346681aa64f627e45eddcc3b4b71cf9dee94a4d6
Fikri <fikriihsan003@yahoo.com>
2016-04-19 22:26:49 +0700

    edit data diri,SC,SRC

---
+++ src/MainRuang2D.java
@@ -0,0 +1,14 @@
+
+
+
+public class MainRuang2D {
+    public static void main(String[] args) {
+        System.out.println("Luas Segitiga : "+Ruang2D.luasSegitiga(4, 9));
+        System.out.println("Keliling Segitiga : "+Ruang2D.KelSegitiga(3));
+        System.out.println("Luas Segi Empat : "+Ruang2D.luasSegiempat(8, 8));
+        System.out.println("Keliling Segi Empat : "+Ruang2D.KelSegiempat(9, 1));
+        System.out.println("Luas Lingkaran : "+Ruang2D.luasLingkaran(10));
+        System.out.println("Keliling Lingkarang : "+Ruang2D.KelLingkaran(5));
+    }
+    
+}
