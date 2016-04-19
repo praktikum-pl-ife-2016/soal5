ba26226c55d4ac5f65bef74d590502b002d32c4e
wunselan <wunselan@gmail.com>
2016-04-19 08:29:19 +0700

    upload mainClass

---
+++ src/MainRuang2D.java
@@ -0,0 +1,14 @@
+public class MainRuang2D {
+
+    public static void main(String[] args) {
+        System.out.println("Keliling Segitiga : " + Ruang2D.kelSegitiga(2, 9));
+        System.out.println("Luas Segitiga     : " + Ruang2D.luasSegitiga(4, 3));
+        System.out.println("=====================================================");
+        System.out.println("Keliling Segiempat : " + Ruang2D.kelSegiEmpat(4, 7));
+        System.out.println("Luas Segiempat     : " + Ruang2D.luasSegiEmpat(6, 5));
+        System.out.println("=====================================================");
+        System.out.println("Keliling Lingkaran  : " + Ruang2D.kelLingkaran(5));
+        System.out.println("Luas Lingkaran      : " + Ruang2D.luasLingkaran(5));
+    }
+    
+}
